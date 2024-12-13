ec8e2b5c48a6f16d0de8f060c9456dcade5475f6
PERCENTZERO <percentzero@outlook.com>
2024-12-13 13:48:15 +0900

    sunrinPASS 웹

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import { type NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
